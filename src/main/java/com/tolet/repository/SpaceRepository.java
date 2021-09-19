7e4322e3eead095fcaabb9a3d9a64afc15b64894
rajeeb <rajeeb@bitmascot.com>
2021-09-19 23:57:37 +0600

    using query param filter the spaces by monthly rent

--- src/main/java/com/tolet/repository/SpaceRepository.java
+++ src/main/java/com/tolet/repository/SpaceRepository.java
@@ -2,6 +2,7 @@ package com.tolet.repository;
 
 import com.tolet.model.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +13,8 @@ public interface SpaceRepository extends JpaRepository<Space,Integer> {
      List<Space> findByDistrictOrderByArea(String district);
      List<Space> findBySpaceType(String spaceType);
 
+     @Query("select s from Space s where s.monthlyRent = ?1")
+     List<Space> findByMonthlyRentOrAdvancedRent(Integer monthlyRent);
 
 
 }
